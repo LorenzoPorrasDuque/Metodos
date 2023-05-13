ad4f1ea078d34ad0c373de55c7fc975cefe03435
Lorenzo Porras Duque <lorenzo.porras.duque@gmail.com>
2023-05-12 21:18:11 -0500

    ·

--- Poisson.py
+++ Poisson.py
@@ -3,21 +3,18 @@ import scipy.stats
 import csv
 import numpy as np
 
-""" mu = 5
-scipy.stats.poisson.rvs(mu,size=1000) 
- """
-data = []
-
-
-with open("customers_per_day.csv") as file:
-    reader = csv.reader(file, delimiter=',')
-    for row in reader:
-        data.append(float(row[0]))
+data = scipy.stats.poisson.rvs(5, size=100)
 
 
 dist = scipy.stats.poisson
 bounds = [(-100, 100)]
 res = scipy.stats.fit(dist, data, bounds)
 print(res)
+
+kstest_result = scipy.stats.kstest(data, 'poisson', args=[5.01649968204578])
+
+print("KS test p-value:", kstest_result[1])
+
+
 res.plot()
 plt.show()
