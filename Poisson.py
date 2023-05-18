ffd6b6f1b33c3daaaab2d9c7608f56c0287100d2
LorenzoPorrasDuque <lorenzo.porras.duque@gmail.com>
2023-05-17 19:41:24 -0500

    #

--- Poisson.py
+++ Poisson.py
@@ -1,15 +1,12 @@
 from scipy.stats import expon, kstest
 import matplotlib.pyplot as plt
 import scipy.stats
-import csv
 import numpy as np
 
-data = scipy.stats.poisson.rvs(7, size=25)
+data = scipy.stats.poisson.rvs(7, size=15)
 dist = scipy.stats.poisson
 bounds = [(-100, 100)]
 res = scipy.stats.fit(dist, data, bounds)
 print(res)
 kstest_result = scipy.stats.kstest(data, 'poisson', args=[res.params[0]])
 print("KS test p-value:", kstest_result[1])
-res.plot()
-plt.show()
